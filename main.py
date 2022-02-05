85559fb8d4105ac4392e17efaa66c41a4ff68c80
jpoirierlavoie <jpoirierlavoie@gmail.com>
2022-02-04 23:30:37 -0500

    Modified flask routes

--- main.py
+++ main.py
@@ -20,17 +20,5 @@ def add_security_headers(response):
 def home():
     return render_template('index.html')
 
-@app.route('/manifest.webmanifest')
-def manifest():
-    return app.send_static_file('manifest.webmanifest'), 200, {'Content-Type': 'application/manifest+json'}
-
-@app.route('/robots.txt')
-def crawler():
-    return app.send_static_file('robots.txt'), 200, {}
-
-@app.route('/sitemap.xml')
-def sitemap():
-    return app.send_static_file('sitemap.xml'), 200, {'Content-Type': 'text/xml; charset=utf-8'}
-
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
