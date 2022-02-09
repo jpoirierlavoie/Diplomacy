565d875ae5eca21bdcdbb04663c2d290889789a3
jpoirierlavoie <jpoirierlavoie@gmail.com>
2022-02-09 08:52:29 -0500

    Added routing for turns

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/turn/<turn>')
+def turn(turn):
+    return render_template("/turn/" + turn + ".html")
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
