fecd0f940bdcf01ad80cffc15da38f33e5bfbf21
jpoirierlavoie <jpoirierlavoie@gmail.com>
2022-02-09 13:20:44 -0500

    added contact page

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def home():
 
 @app.route('/turn/<turn>')
 def turn(turn):
-    return render_template("/turn/" + turn + ".html")
+    return render_template('/turn/' + turn + '.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
