fdac7f9dbd51cf3c425485a8579c73ade5803f09
jpoirierlavoie <jpoirierlavoie@gmail.com>
2022-02-10 09:23:35 -0500

    added security headers

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ turns = {
     
 app = Flask(__name__)
 
+@app.after_request
+def add_security_headers(response):
+    response.headers['X-Frame-Options']='DENY'
+    response.headers['X-content-type-options']='nosniff'
+    response.headers['Referrer-Policy']='no-referrer'
+    response.headers['X-xss-protection']='1; mode=block; report=https://jpoirierlavoie.report-uri.com/r/d/xss/enforce'
+    response.headers['Strict-Transport-Security']='max-age=31536000; includeSubDomains; preload'
+    response.headers['Expect-CT']='max-age=31536000, enforce, report-uri=\"https://jpoirierlavoie.report-uri.com/r/d/ct/enforce\"'
+    response.headers['Report-To']='{"group":"default","max_age":31536000,"endpoints":[{"url":"https://jpoirierlavoie.report-uri.com/a/d/g"}],"include_subdomains":true}'
+    response.headers['NEL']='{"report_to":"default","max_age":31536000,"include_subdomains":true}'
+    response.headers['Content-Security-Policy']='default-src \'self\'; img-src \'self\' data:; script-src-elem \'unsafe-inline\' https://cdn.jsdelivr.net; style-src-elem \'unsafe-inline\' https://cdn.jsdelivr.net; report-uri https://jpoirierlavoie.report-uri.com/r/d/csp/enforce'
+    return response
+
 @app.route('/home')
 @app.route('/')
 def home():
